993f1f139c7872fd2c963ac7ce16d62ab0805b1e
naimiliu <80859263+naimiliu@users.noreply.github.com>
2026-06-08 13:46:44 +0800

    fix

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         文字選取工具箱
 // @namespace    https://github.com/naimiliu/text-selection-toolbox
-// @version      1.0.15.9
+// @version      1.0.15.11
 // @description  文字選取後,顯示命令列
 // @icon         https://raw.githubusercontent.com/naimiliu/text-selection-toolbox/main/options.svg
 // @author       naimiliu
@@ -197,19 +197,20 @@
                             let translatedResult = "";
                             data[0].forEach(row => {
                                 if (row & row[0]) {
-                                    const rawLine = row[0].trim();
+                                    const rawLine = row[0];
+console.log('rowLine:',rawLine)
                                     // 使用正則表達式，把英文單字或個別中文字切開
                                     // \w+'?\w* 代表英文單字(含don't), [\u4e00-\u9fa5] 代表中文字
                                     const tokens = rawLine.split(/(\w+'?\w*|[\u4e00-\u9fa5]|\s+)/g);
                                     tokens.forEach(token => {
-                                        //if (!token) return;
+                                        if (!token) return;
                                         // 只要不是純空白或換行，就用 span 包起來，並加上一個識別 class
                                         if (token.trim().length > 0) {
                                             translatedResult += `<span class="hover-word">${token}</span>`;
                                         } else {
                                             translatedResult += token; // 空白或換行直接保留
                                         }
-console.log(token);
+console.log('token', token);
                                     });
                                 }
                             });
